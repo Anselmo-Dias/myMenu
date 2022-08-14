43d93ce72ecc591f8574b6e5e5d487e1ef87fde7
Anselmo <96529532+Anselmo-Dias@users.noreply.github.com>
2022-08-14 20:22:31 -0300

    commit

--- main.js
+++ main.js
@@ -19,28 +19,50 @@ function showButtomFloat() {
 
 function changeLayout() {
     const elementBody = document.querySelector('body');
-    const elementButton = document.querySelector('button svg');
+    const elementButton = document.querySelector('button');
+    const elementSvg = document.querySelector('button svg');
     const elementH1 = document.querySelector('header h1');
-    const elementSpan = document.querySelector('header  span');
+    const elementSpan = document.querySelector('header span');
     const elementH2 = document.querySelector('section h2');
-    // const ElementLi2 = document.querySelector('button');
-    // const ElementSpan = document.querySelector('span');
-    // const ElementFooter = document.querySelector('footer'); 
+    const elementSection2 = document.querySelector('.section2 h2');
+    const elementSection3 = document.querySelector('.section3 h2');
+    const elementP = document.querySelector('section ul li div p');
+    const elementP2 = document.querySelector('.section2  li div p');
+    const elementP3 = document.querySelector('.section3  li div p');
+    
 
 
     elementBody.classList.add('newStyle')
     elementButton.classList.add('newStyle')
+    elementSvg.classList.add('newStyle')
     elementH1.classList.add('newStyle')
     elementSpan.classList.add('newStyle')
     elementH2.classList.add('newStyle')
+    elementSection2.classList.add('newStyle');
+    elementSection3.classList.add('newStyle');
+    elementP.classList.add('newStyle')
+    elementP2.classList.add('newStyle')
+    elementP3.classList.add('newStyle')
+
 
     setTimeout(function() {
         elementBody.classList.remove('newStyle')
         elementButton.classList.remove('newStyle')
+        elementSvg.classList.remove('newStyle')
         elementH1.classList.remove('newStyle')
+        elementSection2.classList.remove('newStyle');
+        elementSection3.classList.remove('newStyle');
         elementSpan.classList.remove('newStyle')
         elementH2.classList.remove('newStyle')
-    }, 5000)
+        elementP.classList.remove('newStyle')
+        elementP2.classList.remove('newStyle')
+        elementP3.classList.remove('newStyle')
+
+    }, 9000)
+
+  //   setTimeout(function() {
+  //   elementSpan.classList.add('newStyle2')
+  // }, 3000)
 }
 
 
